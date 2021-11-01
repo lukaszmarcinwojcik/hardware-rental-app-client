be41519a7020fe0d31704109c24a1a62a749bb50
Ukasz <lukaszmarcinwojcik@gmail.com>
2021-11-01 10:36:46 +0100

    init pages & routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+
+import React from "react";
+import { useState } from "react";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Dashboard}></Route>
+          <Route exact path="/register" component={Register}></Route>
+          <Route exact path="/login" component={Login}></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Register.js
@@ -0,0 +1,9 @@
+function Register() {
+  return (
+    <div>
+      <h1>Register PAGES</h1>
+    </div>
+  );
+}
+
+export default Register;
