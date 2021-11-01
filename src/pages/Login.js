f5dfad2dba47e19cad4792e29342270f0634841d
Ukasz <lukaszmarcinwojcik@gmail.com>
2021-11-01 11:59:30 +0100

    login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,71 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const API = "https://";
+
+const Error = (props) => <p className="error">{props.error}</p>;
+
 function Login() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState({
+    error: "jakis blad",
+    emailError: "zly email",
+    passwordError: "zle haslo",
+  });
+
+  const handleInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    console.log("name: ", name, "value: ", value);
+
+    setUser({ ...user, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <h1>Login PAGES</h1>
+    <div className={"formContainer"}>
+      <h2>Welcome back</h2>
+      <form action="" onSubmit={handleSubmit}>
+        <div className="loginPanelDiv">
+          <label htmlFor="email">Email</label>
+          <br />
+          <input
+            type="email"
+            value={user.email}
+            onChange={handleInput}
+            name="email"
+            id="email"
+          />
+          {errors.emailError && <Error error={errors.emailError} />}
+        </div>
+        <div className="loginPanelDiv">
+          <label htmlFor="password">Password</label>
+          <br />
+          <input
+            type="password"
+            value={user.password}
+            onChange={handleInput}
+            name="password"
+            id="password"
+          />
+          {errors.passwordError && <Error error={errors.passwordError} />}
+          {errors.error && <Error error={errors.error} />}
+        </div>
+        <button className={"submitBtn"} onClick={handleSubmit}>
+          Login
+        </button>
+      </form>
+
+      <p>
+        No account? <Link to="/register">Register</Link>
+      </p>
     </div>
   );
 }
